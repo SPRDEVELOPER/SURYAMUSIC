d1ba14423b845060b9bded0ff431497fe04c9227
SURYA PRAKASH <mr.suryaprakashraj@gmail.com>
2025-08-13 07:12:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -387,7 +387,13 @@ async def start_handler(_, message):
 
     buttons = [
         [
-        
+            InlineKeyboardButton(f"➕ {add_me_text}", url=f"{BOT_LINK}?startgroup=true"),
+            InlineKeyboardButton(f"📢 {updates_text}", url="https://t.me/vibeshiftbots")
+        ],
+        [
+            InlineKeyboardButton(f"💬 {support_text}", url="https://t.me/Frozensupport1"),
+            InlineKeyboardButton(f"❓ {help_text}", callback_data="show_help")
+        ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
 
@@ -436,6 +442,13 @@ async def go_back_callback(_, callback_query):
 
     buttons = [
         [
+            InlineKeyboardButton(f"➕ {add_me_text}", url=f"{BOT_LINK}?startgroup=true"),
+            InlineKeyboardButton(f"📢 {updates_text}", url="https://t.me/vibeshiftbots")
+        ],
+        [
+            InlineKeyboardButton(f"💬 {support_text}", url="https://t.me/Frozensupport1"),
+            InlineKeyboardButton(f"❓ {help_text}", callback_data="show_help")
+        ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
 
